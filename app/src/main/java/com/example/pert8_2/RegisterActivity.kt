7c708f6266adb8be7fd63789937eb29449f11114
unknown <jidaneadir11@gmail.com>
2022-12-05 08:42:43 +0700

    Update , kurang toast

--- app/src/main/java/com/example/pert8_2/RegisterActivity.kt
+++ app/src/main/java/com/example/pert8_2/RegisterActivity.kt
@@ -1,12 +1,20 @@
 package com.example.pert8_2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
-import androidx.core.text.trimmedLength
+import androidx.appcompat.app.AppCompatActivity
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.error.ANError
+
+import com.androidnetworking.interfaces.JSONObjectRequestListener
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.text.DateFormat.MEDIUM
+
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var username : EditText
@@ -38,8 +46,39 @@ class RegisterActivity : AppCompatActivity() {
             }else if(checkPass.isEmpty()){
                 pass.error = " Password Empty"
             }else{
-                Toast.makeText(this, "Anda berhasil Register", Toast.LENGTH_LONG).show()
+                val jsonObject = JSONObject()
+                try {
+                    jsonObject.put("Name", username)
+                    jsonObject.put("Email", email)
+                    jsonObject.put("Password", pass)
+                    jsonObject.put("terms", 1)
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+
+                AndroidNetworking.post("https://grocery-api.tonsu.site/auth/register")
+                    .addJSONObjectBody(jsonObject) // posting json
+                    .addHeaders("Content-Type", "application/json")
+                    .setPriority(com.androidnetworking.common.Priority.MEDIUM)
+                    .build()
+                    .getAsJSONObject(object : JSONObjectRequestListener {
+                        override fun onResponse(response: JSONObject?) {
+                            try {
+                                Toast.makeText(this@RegisterActivity, "Register Berhasil", Toast.LENGTH_SHORT).show()
+                            }catch (e : JSONException){
+
+                            }
+                        }
+
+                        override fun onError(error: ANError?) {
+                            // handle error
+                        }
+                    })
             }
         }
+    }
+
+    private fun registerUser (){
+
     }
 }
